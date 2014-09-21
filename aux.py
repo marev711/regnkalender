c945789c4bddd158b18eb6cb0e14b1e362100edb
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2014-09-21 10:07:04 +0200

    Refactored python scripts with ./map-color-to-mm-range.py as main script

--- aux.py
+++ aux.py
@@ -1,15 +1,22 @@
-import struct
+import colormath
+import colormath.color_objects
+import csv
 import Image
+import pdb
 import scipy
-import scipy.misc
 import scipy.cluster
+import scipy.misc
+import struct
 import sys
+
+from colormath.color_conversions import convert_color
+from colormath.color_diff import delta_e_cie1976
+from operator import itemgetter
 
 def get_dominant_color(image_path):
     NUM_CLUSTERS = 5
 
-    image = sys.argv[1]
-    im = Image.open(image)
+    im = Image.open(image_path)
     im = im.resize((150, 150))      # optional, to reduce time
     ar = scipy.misc.fromimage(im)
     shape = ar.shape
@@ -24,3 +31,34 @@ def get_dominant_color(image_path):
     peak = codes[index_max]
     color = ''.join(chr(c) for c in peak).encode('hex')
     return color
+
+
+class labrange:
+    def __init__(self, hex_rgb, vrange):
+        srgb = colormath.color_objects.sRGBColor(0,0,0)
+        self.lab = convert_color(srgb.new_from_rgb_hex(hex_rgb), colormath.color_objects.LabColor)
+        self.vrange = vrange
+        self.delta_e = 1000000
+
+    def distance(self, lab_cf):
+        self.delta_e = delta_e_cie1976(self.lab, lab_cf)
+        
+
+def keydef(x):
+    return x.delta_e
+
+# Read refernce list
+def color2mm_range(hex_rgb):
+    f = open("colors_in_map_colorbar_rgb_hex.txt", "r")
+    crange_list = list(csv.reader(f, delimiter='\t'))
+    crange = [labrange(cr[0].split()[0], cr[0].split()[1]) for cr in crange_list]
+    
+    # Read requested value
+    srgb = colormath.color_objects.sRGBColor(0,0,0)
+    lab_hexed = convert_color(srgb.new_from_rgb_hex(hex_rgb), colormath.color_objects.LabColor)
+    
+    for labr in crange:
+        labr.distance(lab_hexed)
+    
+    crange_sorted = sorted(crange, key=keydef)
+    return crange_sorted[0].vrange
--- map-color-to-mm-range.py
+++ map-color-to-mm-range.py
@@ -1,20 +1,28 @@
 #! /usr/bin/env python
 import os
-from get_dominant_color import get_dominant_color
+from aux import get_dominant_color
+from aux import color2mm_range
 import re
 import sys
 
 date_regexp_str = '.*_([0-9]{6})-.*'
 
 if __name__ == "__main__":
-    date_regexp = re.compile(date_regexp_str)
     fig_dir = sys.argv[1]
+    date_regexp = re.compile(date_regexp_str)
     figs = os.listdir(fig_dir)
     fig_dates = dict([(date_regexp.search(fig).group(1), fig) for fig in figs])
     fig_dates_sorted = sorted(fig_dates)
     figs_sorted = [fig_dates[date] for date in fig_dates_sorted]
-    print figs_sorted
 
+    date_mm_range = {}
+    for fig_file in [os.path.join(fig_dir, fig) for fig in figs_sorted]:
+        hex_rgb = get_dominant_color(fig_file)
+        mm_range = color2mm_range(hex_rgb)
+
+        curr_date = date_regexp.search(fig_file).group(1)
+        date_mm_range[curr_date] = mm_range
+    print date_mm_range
     sys.exit(1)
 
 #    max_color=$(./get_dominant_color.py ${figure_folder}/rrdm_1409${fil}-cropped.jpg)
