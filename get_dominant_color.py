d8c339b6118cd0f5a8496e6e591e790c0ad901c6
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2014-09-21 09:53:23 +0200

    Refactored sub scripts to be call/be called by others

--- get_dominant_color.py
+++ get_dominant_color.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python
 import struct
 import Image
 import scipy
@@ -6,21 +5,22 @@ import scipy.misc
 import scipy.cluster
 import sys
 
-NUM_CLUSTERS = 5
+def get_dominant_color(image_path):
+    NUM_CLUSTERS = 5
 
-image = sys.argv[1]
-im = Image.open(image)
-im = im.resize((150, 150))      # optional, to reduce time
-ar = scipy.misc.fromimage(im)
-shape = ar.shape
-ar = ar.reshape(scipy.product(shape[:2]), shape[2])
+    image = sys.argv[1]
+    im = Image.open(image)
+    im = im.resize((150, 150))      # optional, to reduce time
+    ar = scipy.misc.fromimage(im)
+    shape = ar.shape
+    ar = ar.reshape(scipy.product(shape[:2]), shape[2])
 
-codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
 
-vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
-counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+    vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+    counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
 
-index_max = scipy.argmax(counts)                    # find most frequent
-peak = codes[index_max]
-colour = ''.join(chr(c) for c in peak).encode('hex')
-print '#%s' % (colour)
+    index_max = scipy.argmax(counts)                    # find most frequent
+    peak = codes[index_max]
+    color = ''.join(chr(c) for c in peak).encode('hex')
+    return color
